f85b7d60e13fd24776b847f2a7aef37872469d56
Matheus Vieira <38119742+vssmatheus@users.noreply.github.com>
2023-04-24 00:24:40 -0300

    feat(abertura): adicionanr modulo de abertura (#2)
    * feat(abertura): adicionanr modulo de abertura
    
    Adicionando modulo de abertura
    
    * feat(abertura): adicionando modulo de abertura
    
    * feat(abertura-header): adicionar button de retornar no header

--- src/app/pages/abertura/abertura.component.ts
+++ src/app/pages/abertura/abertura.component.ts
@@ -5,4 +5,31 @@ import { Component } from '@angular/core';
   templateUrl: './abertura.component.html',
   styleUrls: ['./abertura.component.scss'],
 })
-export class AberturaComponent {}
+export class AberturaComponent {
+  buttons = [
+    {
+      name: 'Material didático',
+      image: '../../../assets/abertuta/material-didatico.svg',
+      route: '/login',
+      color: '#4173CD',
+    },
+    {
+      name: 'Quiz',
+      image: '../../../assets/abertuta/quiz.svg',
+      route: '/#',
+      color: '#1DC763',
+    },
+    {
+      name: 'Desenho',
+      image: '../../../assets/abertuta/desenho.svg',
+      route: '/#',
+      color: '#F6B5FF',
+    },
+    {
+      name: 'Youtube',
+      image: '../../../assets/abertuta/youtube.svg',
+      route: '/#',
+      color: '#FF0000',
+    },
+  ];
+}
--- src/app/pages/abertura/abertura.module.ts
+++ src/app/pages/abertura/abertura.module.ts
@@ -1,11 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { HeaderComponent } from 'src/app/shared/header/header.component';
 import { AberturaRoutingModule } from './abertura-routing.module';
 import { AberturaComponent } from './abertura.component';
+import { ButtonNavigatorAberturaComponent } from './components/button-navigator-abertura/button-navigator-abertura.component';
 
 @NgModule({
-  declarations: [AberturaComponent],
+  declarations: [
+    AberturaComponent,
+    ButtonNavigatorAberturaComponent,
+    HeaderComponent,
+  ],
   imports: [CommonModule, AberturaRoutingModule],
 })
 export class AberturaModule {}
